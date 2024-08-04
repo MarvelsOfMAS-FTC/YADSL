5813d4998b860761ab4c14754fd8a8e44f312ce1
IKKNIGHT <76535471+IKKNIGHT@users.noreply.github.com>
2024-08-03 22:48:53 -0500

    stuff

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Constants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Constants.java
@@ -11,9 +11,10 @@ public class Constants {
             RevHubOrientationOnRobot.UsbFacingDirection.FORWARD));
 
     //Names for motors and servos
-    public static final String drone = "drone", rSlide = "rightSlide", lSlide = "leftSlide", fr = "frontRight", fl = "frontLeft", br = "backRight", bl = "backLeft", hand = "hand", intake = "intake";
+    public static final String drone = "drone", rSlide = "rightSlide", lSlide = "leftSlide", fr = "frontRight", fl = "frontLeft", br = "backRight", bl = "backLeft", hand = "hand", intake = "intake", imu = "imu";
     //Hand Pos
     public static final double out = 0, in = 0;
     //Drone Pos
     public static final double launch = 0, load = 0;
+
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/Solo.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/Solo.java
@@ -1,13 +1,16 @@
 package org.firstinspires.ftc.teamcode.teleop;
 
 import com.arcrobotics.ftclib.command.CommandOpMode;
+import com.arcrobotics.ftclib.command.InstantCommand;
 import com.arcrobotics.ftclib.command.button.Button;
 import com.arcrobotics.ftclib.command.button.GamepadButton;
 import com.arcrobotics.ftclib.gamepad.GamepadEx;
 import com.arcrobotics.ftclib.gamepad.GamepadKeys;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
+import org.firstinspires.ftc.robotcontroller.external.samples.ConceptScanServo;
 import org.firstinspires.ftc.teamcode.Constants;
+import org.firstinspires.ftc.teamcode.commands.DriveCommand;
 import org.firstinspires.ftc.teamcode.commands.IntakeCommand;
 import org.firstinspires.ftc.teamcode.commands.SlideCommand;
 import org.firstinspires.ftc.teamcode.subsystems.Drive;
@@ -15,53 +18,25 @@ import org.firstinspires.ftc.teamcode.subsystems.DroneSubsystem;
 import org.firstinspires.ftc.teamcode.subsystems.HandSubsystem;
 import org.firstinspires.ftc.teamcode.subsystems.IntakeSubsystem;
 import org.firstinspires.ftc.teamcode.subsystems.SlideSubsystem;
+import org.firstinspires.ftc.teamcode.utils.MotorConfig;
+import org.firstinspires.ftc.teamcode.utils.MotorDirectionConfig;
 
 public class Solo extends CommandOpMode {
     @Override
     public void initialize() {
-        GamepadEx drive = new GamepadEx(gamepad1);
+        GamepadEx base = new GamepadEx(gamepad1);
+
+        IntakeSubsystem intake = new IntakeSubsystem(hardwareMap, Constants.intake);
+        Drive drive = new Drive(hardwareMap, Constants.imu,new MotorConfig(Constants.fr,Constants.fl,Constants.br,Constants.bl),new MotorDirectionConfig(false,false,false,false));
         /*
-        IntakeSubsystem intake = new IntakeSubsystem(HardwareMap, Constants.intake);
         DroneSubsystem drone = new DroneSubsystem();
         HandSubsystem hand = new HandSubsystem();
         SlideSubsystem slide = new SlideSubsystem();
         */
-        Button a1 = new GamepadButton(
-                drive, GamepadKeys.Button.A
-        );
-        Button b1 = new GamepadButton(
-                drive, GamepadKeys.Button.B
-        );
-        Button x1 = new GamepadButton(
-                drive, GamepadKeys.Button.X
-        );
-        Button y1 = new GamepadButton(
-                drive, GamepadKeys.Button.Y
-        );
-        Button up1 = new GamepadButton(
-                drive, GamepadKeys.Button.DPAD_UP
-        );
-        Button left1 = new GamepadButton(
-                drive, GamepadKeys.Button.DPAD_LEFT
-        );
-        Button right1 = new GamepadButton(
-                drive, GamepadKeys.Button.DPAD_RIGHT
-        );
-        Button down1 = new GamepadButton(
-                drive, GamepadKeys.Button.DPAD_DOWN
-        );
-        Button rightB1 = new GamepadButton(
-                drive, GamepadKeys.Button.RIGHT_BUMPER
-        );
-        Button leftB1 = new GamepadButton(
-                drive, GamepadKeys.Button.LEFT_BUMPER
-        );
-        Button leftS1 = new GamepadButton(
-                drive, GamepadKeys.Button.LEFT_STICK_BUTTON
-        );
-        Button rightS1 = new GamepadButton(
-                drive, GamepadKeys.Button.RIGHT_BUMPER
-        );
-        rightB1.whenPressed(new IntakeCommand(intake, 1))
+
+
+
+        // default commands...
+        drive.setDefaultCommand(new DriveCommand(drive,base));
     }
 }

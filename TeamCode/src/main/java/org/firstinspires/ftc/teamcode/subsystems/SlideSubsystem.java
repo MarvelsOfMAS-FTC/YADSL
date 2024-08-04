7d2dc192bab99f8a1f28c12a182bddf9ff2ed5b7
IK_Knight <76535471+IKKNIGHT@users.noreply.github.com>
2024-08-03 19:10:51 -0500

    rge

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/SlideSubsystem.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/SlideSubsystem.java
@@ -1,5 +1,6 @@
 package org.firstinspires.ftc.teamcode.subsystems;
 import com.arcrobotics.ftclib.command.SubsystemBase;
+import com.qualcomm.robotcore.hardware.DcMotorSimple.Direction
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
@@ -8,9 +9,11 @@ public class SlideSubsystem extends SubsystemBase {
      * Creates a new ExampleSubsystem.
      */
     private final DcMotorEx right, left;
-    public SlideSubsystem(HardwareMap h, String right, String left) {
+    public SlideSubsystem(HardwareMap h, String right, String left, Direction rightD, Direction leftD) {
         this.right = h.get(DcMotorEx.class, right);
         this.left = h.get(DcMotorEx.class, left);
+        this.right.setDirection(rightD);
+        this.left.setDirection(leftD);
     }
     public void set(double speed) {
         this.right.setPower(speed);

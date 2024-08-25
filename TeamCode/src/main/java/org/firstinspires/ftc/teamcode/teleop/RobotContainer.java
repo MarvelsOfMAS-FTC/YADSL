6334fee7d87e0fe62d9466b24b4f813348cb9002
NoahMarvelsofMAS <88335646+NoahMarvelsofMAS@users.noreply.github.com>
2024-08-25 17:32:08 -0500

    e

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/RobotContainer.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/RobotContainer.java
@@ -1,7 +1,6 @@
 package org.firstinspires.ftc.teamcode.teleop;
 
 import com.arcrobotics.ftclib.command.CommandOpMode;
-import com.arcrobotics.ftclib.command.RunCommand;
 import com.arcrobotics.ftclib.gamepad.GamepadEx;
 
 import org.firstinspires.ftc.teamcode.commands.DriveCommand;
@@ -9,12 +8,12 @@ import org.firstinspires.ftc.teamcode.subsystems.Drive;
 import org.firstinspires.ftc.teamcode.utils.MotorConfig;
 import org.firstinspires.ftc.teamcode.utils.MotorDirectionConfig;
 
-public class Base extends CommandOpMode {
-
+public class RobotContainer extends CommandOpMode {
+    public static Drive drive;
     @Override
     public void initialize() {
         GamepadEx base = new GamepadEx(gamepad1);
-        Drive drive = new Drive(hardwareMap,"imu",new MotorConfig("fr","fl","br","bl"),new MotorDirectionConfig(false,false,false,false));
+        drive = new Drive(hardwareMap,"imu",new MotorConfig("fr","fl","br","bl"),new MotorDirectionConfig(false,false,false,false));
         drive.setDefaultCommand(new DriveCommand(drive,base));
     }
 }
